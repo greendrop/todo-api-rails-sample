6123b0bef6d3ca728952d8cd57e49bb1fef4474c
greendrop <greendrop.devel@gmail.com>
2021-01-23 14:41:10 +0900

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   end
 
   # GraphQL
-  post 'api/v1/graphql', to: 'api/v1/graphql#execute'
+  post 'api/graphql', to: 'api/graphql#execute'
 
   get '/home', to: 'homes#index'
   root to: 'homes#index'
